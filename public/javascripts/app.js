ba4d2f0d1d35255fc95ace866ffcd10be82159e2
Peter-White <pwhite007@icloud.com>
2016-11-24 20:45:39 -0500

    div click hide random test

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -35,16 +35,23 @@ $(document).ready(function() {
     function displayProjects(data) {
       var projectHTML = '';
       $.each(data, function(i, project) {
-        projectHTML += '<li><p id="summary">' + project.summary + '</p></li>';
-        // projectHTML += '<li><p id="technologies">' + project.technologies + '</p></li>';
-        // projectHTML += '<li><a href="' + project.url + '" id="url">' + project.url + '</a></li>';
-        // projectHTML += '<li><a href="' + project.github + '" id="github">' + project.github + '</a></li>';
-        // projectHTML += '<li><h2 id="desktop">' + project.desktop + '</h2></li>';
-        // projectHTML += '<li><h2 id="mobile">' + project.mobile + '</h2></li>';
+        projectHTML += '<p id="summary">' + project.summary + '</p>';
+        projectHTML += '<p id="technologies">' + project.technologies + '</p>';
+        projectHTML += '<a href="' + project.url + '" id="url">' + project.url + '</a>';
+        projectHTML += '<a href="' + project.github + '" id="github">' + project.github + '</a>';
+        if(project.desktop === true) {
+          projectHTML += '<h2 id="desktop">' + project.desktop + '</h2>';
+        }
+        if(project.mobile === true) {
+          projectHTML += '<h2 id="mobile">' + project.mobile + '</h2>';
+        }
       });
-      $this.append(projectHTML);
+      $('.extraInfo').html(projectHTML);
     };
     $.getJSON(projectAPI, projectOptions, displayProjects);
+  });
+  $('.extraInfo').click(function(){
+    $(this).html("");
   });
 });
 
