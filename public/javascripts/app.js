59012a95cfd8bb9ef06d18e274b408f70c181922
Peter-White <pwhite007@icloud.com>
2016-12-12 17:07:04 -0500

    Trying new post method, I'm scared

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,57 +1,49 @@
 $(document).ready(function() {
 
-  var projectAPI = "/projects";
-  var projectOptions = {
-    format: "json"
-  };
+  var $submit = $('.submit');
+  var $desktop = $('.desktop');
+  var $mobile = $('.mobile');
 
-  function displayProjects(data) {
-    var projectHTML = '<div class="projectContainer">';
-    $.each(data, function(i, project) {
-      projectHTML += '<ul type="none" id="' + project._id + '">';
-      projectHTML += '<li><h2 id="title">' + project.title + '</h2></li>';
-      projectHTML += '<li><img id="image" src="' + project.image + '" height="300px"></li>';
-      projectHTML += '</ul>';
-      projectHTML += '<br>';
+  function setBoolean(selector){
+    if(selector.val() === "true") {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  function boxCheck() {
+    var checkedArray = [];
+    $.each($("input:checkbox:checked"), function(){
+      checkedArray.push($(this).val());
     });
-    projectHTML += '</div>';
-    $('.projectList').html(projectHTML);
+    if(checkedArray.length === 0) {
+      return "error";
+    } else {
+    return checkedArray;
+    }
   };
-  $.getJSON(projectAPI, projectOptions, displayProjects);
-});
+  function validate() {
+    var $input = $("input:text");
+    for(var i = 0; i < $input.length; i++) {
+      if($input[i].value === "") {
+        return false;
+      }
+    }
+    return true;
+  };
 
-$(document).ready(function() {
-  // This is how you interact with a generated element
-  $('.projectList').on('click', '.projectContainer ul' , function(event){
-    // Assign a variable to "this" before using it in an ajax request
-    var $this = $(this);
-    var listId = $this.attr("id");
-
-    var projectAPI = "/projects/" + listId;
-    var projectOptions = {
-      format: "json",
-    };
-
-    function displayProjects(data) {
-      var projectHTML = '';
-      $.each(data, function(i, project) {
-        projectHTML += '<p id="summary">' + project.summary + '</p>';
-        projectHTML += '<p id="technologies">' + project.technologies + '</p>';
-        projectHTML += '<a href="' + project.url + '" id="url">' + project.url + '</a>';
-        projectHTML += '<a href="' + project.github + '" id="github">' + project.github + '</a>';
-        if(project.desktop === true) {
-          projectHTML += '<h2 id="desktop">' + project.desktop + '</h2>';
-        }
-        if(project.mobile === true) {
-          projectHTML += '<h2 id="mobile">' + project.mobile + '</h2>';
-        }
-      });
-      $('.extraInfo').html(projectHTML);
-    };
-    $.getJSON(projectAPI, projectOptions, displayProjects);
+  $submit.click(function(event) {
+    /* Act on the event */
+    event.preventDefault();
+    console.log(validate());
+    console.log(boxCheck());
+    console.log($("textarea").val());
+    console.log(setBoolean($desktop));
+    console.log(setBoolean($mobile));
+    // console.log(validate());
   });
-  $('.extraInfo').click(function(){
-    $(this).html("");
-  });
+
+
 });
 
