2123921efc0586df39408653fe2e65a4c926181f
Peter-White <pwhite007@icloud.com>
2016-12-13 00:52:06 -0500

    New Post Method Successful

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
-  var $submit = $('.submit');
   var $desktop = $('.desktop');
   var $mobile = $('.mobile');
+  var $input = $("input:text");
+  var $textarea = $("textarea");
+  var $message = $('.message');
 
   function setBoolean(selector){
     if(selector.val() === "true") {
@@ -11,7 +13,6 @@ $(document).ready(function() {
       return false;
     }
   }
-
   function boxCheck() {
     var checkedArray = [];
     $.each($("input:checkbox:checked"), function(){
@@ -20,30 +21,49 @@ $(document).ready(function() {
     if(checkedArray.length === 0) {
       return "error";
     } else {
-    return checkedArray;
+    var arrString = checkedArray.join(", ")
+    return arrString;
     }
   };
   function validate() {
-    var $input = $("input:text");
     for(var i = 0; i < $input.length; i++) {
       if($input[i].value === "") {
         return false;
       }
     }
+    if(boxCheck() === "error") {
+      return false;
+    }
+    if($textarea.val() === "") {
+      return false;
+    }
     return true;
   };
 
-  $submit.click(function(event) {
-    /* Act on the event */
+  $('form').submit(function(event){
     event.preventDefault();
-    console.log(validate());
-    console.log(boxCheck());
-    console.log($("textarea").val());
-    console.log(setBoolean($desktop));
-    console.log(setBoolean($mobile));
-    // console.log(validate());
+    if(validate() === true) {
+      $message.html('<h3>Success<h3>');
+
+      var postValues = {
+        title: $input[0].value,
+        image: $input[1].value,
+        summary: $textarea.val(),
+        technologies: boxCheck(),
+        url: $input[2].value,
+        github: $input[3].value,
+        desktop: setBoolean($desktop),
+        mobile: setBoolean($mobile)
+      };
+      console.log(postValues);
+      $.post('/projects', postValues, function(response, status) {
+        // optional stuff to do after success
+        $input.val('');
+        $textarea.val('');
+      }, 'json');
+    } else {
+      $message.html('<h3>Validation Failed<h3>');
+    }
   });
-
-
 });
 
